66797f041e27ae5b55d61c3487933a96dad544dd
Al-27 <ahmedlaghfour@gmail.com>
2023-11-26 16:04:11 +0000

    100 tasks

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -8,12 +8,12 @@ def append_after(filename="", search_string="", new_string=""):
     """
     ...
     """
-    
+    newTxt = ""
     with open("text","r+") as file:
-        newTxt = ""
         for line in file:
             newTxt += line
             if search_string in line:
                 newTxt += new_string
-        file.seek(0,0)
+    
+    with open("text","w") as file: 
         file.write(newTxt)
