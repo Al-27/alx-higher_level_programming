458be2c81cfa2ded29e4beb5e8327b6314798c58
Al-27 <ahmedlaghfour@gmail.com>
2023-11-26 19:36:43 +0000

    ___001

---
+++ 0x07-python-test_driven_development/tests/6-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" ... """
+
+def max_integer(list=[]):
+    """
+        ....
+    """
+    if len(list) == 0:
+        return None
+    list.sort()
+    max = list[-1]
+    return max
