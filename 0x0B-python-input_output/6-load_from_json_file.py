22cfd46b99cfbcc03d17f519a499774251475ec1
Al-27 <ahmedlaghfour@gmail.com>
2023-11-26 12:09:47 +0000

    d

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -12,5 +12,6 @@ def load_from_json_file(filename):
     with open(filename, encoding='utf-8') as f:
         str = f.read()
     
+    str = '[]' if str=="" else str 
     return json.loads(str)
         
