49adb297073342949aa9d29d72f67bb96adba407
Al-27 <ahmedlaghfour@gmail.com>
2023-11-26 12:06:37 +0000

    d

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,6 +4,7 @@
     ...
 """
 from sys import argv 
+from os import path
 save = __import__("5-save_to_json_file").save_to_json_file
 load = __import__("6-load_from_json_file").load_from_json_file
 if __name__ == "__main__":
@@ -11,9 +12,11 @@ if __name__ == "__main__":
         ...
         ...
     """
-    fname = "add_item.json" 
-    f = open(fname, "a+")
-    f.close() 
+    fname = "add_item.json"
+    if path.isfile(fname) == False:
+        f = open(fname, "w")
+        f.close()
+    
     obj = load(fname)
     obj += argv[1:] 
     save(obj,fname)
