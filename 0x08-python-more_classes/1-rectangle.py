071fcf5432de21fba194494d6eadc85da717d700
Al-27 <ahmedlaghfour@gmail.com>
2023-11-27 20:11:51 +0000

    ___2711

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,46 +1,45 @@
 #!/usr/bin/python3
 """Rectangle"""
-
-
 class Rectangle:
     """Rectangle"""
-
     def __init__(self, w=0, h=0):
-        self.check_valid(w, h)
+        self.check_valid(w,h)
         self.__width = w
         self.__height = h
-
+    
+    
     def width(self, value=None):
-        if value is None:
+        if value == None:
             return self.__width
-
+         
         if self.check_valid(w=value):
             self.__width = value
-
+            
     def height(self, value=None):
-        if value is None:
+        if value == None:
             return self.__height
-
+        
         if self.check_valid(h=value):
-            self.__height = value
-
-    width = property(width, width)
-    height = property(height, height)
-
+            self.__height = value 
+    
+    
+    width = property(width,width)
+    height = property(height,height)
+    
     def check_valid(self, w=None, h=None):
         mustbe = " must >= 0"
         mustbe2 = " must be an integer"
-        if w is not None:
-            if isinstance(w, int):
+        if w != None:
+            if isinstance(w,int) :
                 return True
-            elif w < 0:
+            elif w < 0 :
                 raise ValueError("width" + mustbe)
             else:
                 raise TypeError("width" + mustbe2)
-        if w is not None:
-            if isinstance(h, int):
+        if h != None:
+            if isinstance(h,int):
                 return True
-            elif h < 0:
+            elif h < 0 :
                 raise ValueError("height" + mustbe)
             else:
                 raise TypeError("height" + mustbe2)
