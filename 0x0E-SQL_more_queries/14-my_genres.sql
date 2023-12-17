03dd97af4f977368fdcb5adcc2bc998726f467c6
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-12-17 07:26:30 +0000

    Update 14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,4 @@
 -- DESC
 SELECT tv_genres.name FROM tv_show_genres 
-    FULL OUTER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id 
-    FULL OUTER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id WHERE tv_shows.title = "Dexter" ORDER BY tv_show_genres.title ASC;
+    RIGHT OUTER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id 
+    RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id WHERE tv_shows.title = "Dexter" ORDER BY tv_show_genres.title ASC;
