abc4d02765cf3f2e5b5cb5b10363d1e2334ed27d
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-01-09 17:44:03 +0000

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
+  let size = parseInt(process.argv[2]);
 if ( isNaN(process.argv[2])) {
   console.log('Missing size');
 } 
 else {
-  let size = parseInt(process.argv[2]);
   let i = 0;
   while (i < size) {
     console.log('X'.repeat(size));
