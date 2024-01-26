84c0ae26337604127be42961882cabce07a0ac9a
Al_27 <ahmedlaghfour@gmail.com>
2024-01-26 02:18:55 +0000

    wc

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # comment
-curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" http://55fa11af441d.9b220f8f.alx-cod.online:5000/route_6
+curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" $1
