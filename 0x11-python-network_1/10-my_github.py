96ea4ca844aab47a154a9576dd5f0dd389b0bfcc
Al_27 <ahmedlaghfour@gmail.com>
2024-01-27 01:39:55 +0000

    wrong file

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,9 +5,4 @@ hippity hoppity, this is a doc
 import requests
 
 
-if __name__ == "__main__":
-    headers = {"Accept": "application/vnd.github+json"}
-    resp = requests.get(
-        "https://api.github.com/repos/rails/rails/commits?per_page=10", headers=headers)
-    for js in resp.json():
-        print(f'{js["sha"]}: {js["commit"]["author"]["name"]}')
+if __name__ == "__main__": 
