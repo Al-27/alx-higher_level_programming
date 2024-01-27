556e3ba5bdffeec8dde4346fe3f5bbef20ad5518
Al_27 <ahmedlaghfour@gmail.com>
2024-01-27 02:24:42 +0000

    tabs

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ import requests
 if __name__ == "__main__":
     resp = requests.get('https://alx-intranet.hbtn.io/status')
     print(f"Body response:")
-    print(f'    - type: {type(resp.text)}')
-    print(f'    - content: {resp.text}')
+    print(f'\t- type: {type(resp.text)}')
+    print(f'\t- content: {resp.text}')
