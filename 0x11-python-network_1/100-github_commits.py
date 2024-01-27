bdabc3af73518f0940236e8a825991ef2749f2e2
Al_27 <ahmedlaghfour@gmail.com>
2024-01-27 01:54:38 +0000

    argv order

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,7 +10,7 @@ import sys.argv
 if __name__ == "__main__":
     headers = {"Accept": "application/vnd.github+json"}
     resp = requests.get(
-        f"https://api.github.com/repos/{argv[1]}/{argv[2]}/commits?per_page=10",
+        f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits?per_page=10",
         headers=headers)
     for js in resp.json():
         print(f'{js["sha"]}: {js["commit"]["author"]["name"]}')
