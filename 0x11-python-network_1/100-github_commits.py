0ae6ad0226d9614fb7219a554d498cedd401620d
Al_27 <ahmedlaghfour@gmail.com>
2024-01-27 02:28:41 +0000

    fixed

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,21 +5,13 @@ hippity hoppity, this is a doc
 --DOCS
 """
 import requests
-import sys.argv
-"""
----
-hippity hoppity, this is a doc
---DOCS
-"""
+import sys
+
 if __name__ == "__main__":
-    """
-    ---
-    hippity hoppity, this is a doc
-    --DOCS
-    """
+
     headers = {"Accept": "application/vnd.github+json"}
     resp = requests.get(
-        f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits?per_page=10",
+        f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits?per_page=10",
         headers=headers)
     for js in resp.json():
         print(f'{js["sha"]}: {js["commit"]["author"]["name"]}')
