6426bdd0cc47a59cea9c8c550f78e5b43323d3ec
Al_27 <ahmedlaghfour@gmail.com>
2024-01-27 00:36:16 +0000

    r

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-
-from sys import argv
-import urllib.request as request
-
 """
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
 Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris  
 """
+from sys import argv
+import urllib.request as request
 
-with request.urlopen(f'{argv[1]}') as response:
-    print( response.headers["X-Request-Id"] )
+if __name__ == "__main__":
+    with request.urlopen(f'{argv[1]}') as response:
+        print( response.headers["X-Request-Id"] )
