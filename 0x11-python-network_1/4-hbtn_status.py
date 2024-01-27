2935ddc6260a64e9de20a6e24452b310a0d0b750
Al_27 <ahmedlaghfour@gmail.com>
2024-01-27 02:12:24 +0000

    tewt

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ import requests
 if __name__ == "__main__":
     resp = requests.get('https://alx-intranet.hbtn.io/status')
     print(f"""Body response:
-    - type: {type(resp.content)}
+    - type: {type(resp.text)}
     - utf8 content: {resp.text}""")
