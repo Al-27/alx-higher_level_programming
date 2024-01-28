22cf0ab0dfcbf8efd8a83ec718f652863df29c31
Al_27 <ahmedlaghfour@gmail.com>
2024-01-28 08:30:48 +0000

    7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,5 @@ const { argv } = require('process');
 let i = parseInt(argv[2]);
 
 if( isNaN(i) ) console.log( 'Missing number of occurrences' );
-else if ( i > 0 )
-{
-    for(let j = 0; j < i ; j++) console.log( 'C is fun' );
-}
+
+for(let j = 0; j < i && !isNaN(i) ; j++) console.log( 'C is fun' );
